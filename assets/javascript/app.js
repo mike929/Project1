94c8a82ba5361e890531314cb3dd86e33c50e33a
mike929 <mike929@gmail.com>
2019-01-06 12:40:03 -0500

    hackedUpMockUp

---
+++ assets/javascript/app.js
@@ -0,0 +1,36 @@
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return; // stop function from running for undefined keys
+    audio.currentTime = 0; // rewind audio to the start
+    audio.play();
+    key.classList.add('playing');
+
+  }
+// TURNING THIS BLOCK OF CODE INTO A FUNCTION - playSound
+  // window.addEventListener('keydown', function (e) {
+  //   // console.log(e.keyCode); to see the keycodes of keys
+  //   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  //   const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+  //   // console.log(audio);  press key to see if it has audio class
+  //   if (!audio) return; // stop function from running for undefined keys
+  //   audio.currentTime = 0; // rewind audio to the start
+  //   audio.play();
+  //   // console.log(key);
+  //   key.classList.add('playing');
+  // });
+
+    function removeTransition(e) {
+      // console.log(e);
+      if (e.propertyName !== 'transform') return; // skip it if it's not a transform
+      // console.log(e.propertyName);
+      // console.log(this);  HOW TO FIND OUT WHAT THIS IS equal TO
+      this.classList.remove('playing');
+    }
+
+
+
+    const keys = document.querySelectorAll('.key');
+    keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+    window.addEventListener('keydown', playSound);
