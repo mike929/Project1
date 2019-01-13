65a4a22e6ebee99b839773d1da77450b51d92d3a
KHAUNA <jtwinturbo@gmail.com>
2019-01-13 15:48:52 -0500

    still working through issues of callbacks

--- assets/javascript/zomatoAPIv2.js
+++ assets/javascript/zomatoAPIv2.js
@@ -1,15 +1,15 @@
 // zomato api key: 0873254cb6494b6d3d3851a49e152e4c
 
-// initial sample query: - need to get city id code
+// initial sample query: - takes city name and returns cityid code and entityType (needed for 2nd call)
 //https://developers.zomato.com/api/v2.1/locations?query=Atlanta&apikey=0873254cb6494b6d3d3851a49e152e4c
 
 
-// second sample query: - returns restaurant info etc..
+// second sample query: - takes api key, cityid, and entity type and returns restaurant info etc..
  // https://developers.zomato.com/api/v2.1/location_details?entity_id=288&entity_type=city&apikey=0873254cb6494b6d3d3851a49e152e4c
 
+ //global variables
 var apiKey = "0873254cb6494b6d3d3851a49e152e4c"
 var userCitySearch = "Atlanta";
-//global variables
 var cityId = "";
 var cityStateName = "";
 var entityType = "";
@@ -17,8 +17,11 @@ var entityId = "";
 var lat = "";
 var long = "";
 
+//1st API call to zomato api - this returns city data that we need to use to call in the 
+//darksky api (lat/long).  This also returns data we need in our second api call to zomato
+// that gives us the restaurant info (we need cityID and entityType in our second call to zomato api)
 var queryURL1 ="https://developers.zomato.com/api/v2.1/locations?query=" + userCitySearch + "&apikey=" + apiKey
-function locationInfoCall(){
+function locationInfoCall(callback){
     $.ajax({
         url: queryURL1,
         method: "GET"
@@ -39,8 +42,8 @@ function locationInfoCall(){
             long: long
         }
         console.log(parameters)
-        console.log(entityType)
     });
+callback();
 }//end locationInfoCall function
 
 // call from controller to invoke search
@@ -65,16 +68,19 @@ function getLatLong(city) {
 
 }
 function foodInfoCall() {
-    locationInfoCall();
-    console.log("Type: " + entityType)
-  var queryURL2 = "https://developers.zomato.com/api/v2.1/location_details?entity_id=" + entityId + "&entity_type=" + entityType + "&apikey=" + apiKey
-  $.ajax({
-    url: queryURL2,
-    method: "GET"
-  }).then(function(zomatoResponse2) {
-      console.log(zomatoResponse2)
-  });
+    console.log("Tis: " + entityId + "from foodInfoCall function")
+//   var queryURL2 = "https://developers.zomato.com/api/v2.1/location_details?entity_id=" + entityId + "&entity_type=" + entityType + "&apikey=" + apiKey
+//   $.ajax({
+//     url: queryURL2,
+//     method: "GET"
+//   }).then(function(zomatoResponse2) {
+//       console.log("entity type is " + entityType)
+//   });
 }//end foodInfoCall function
+
+locationInfoCall(foodInfoCall);
+
+
 
 
 
